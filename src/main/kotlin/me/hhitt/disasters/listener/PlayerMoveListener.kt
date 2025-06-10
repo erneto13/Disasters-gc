77baac25f3fac9c308bfe6ef0630d0b20c082a79
Imhhitt <diegovicentehernandez276@gmail.com>
2025-06-10 17:48:38 +0200

    refactor: improved code another time, yes

--- src/main/kotlin/me/hhitt/disasters/listener/PlayerMoveListener.kt
+++ src/main/kotlin/me/hhitt/disasters/listener/PlayerMoveListener.kt
@@ -13,65 +13,66 @@ class PlayerMoveListener(private val arenaManager: ArenaManager): Listener {
 
     @EventHandler
     fun onPlayerMove(event: PlayerMoveEvent) {
-        if (event.from.block != event.to.block) {
-
-            // If player is in the same block, we don't care and return
-            if (event.from.blockX == event.to.blockX &&
-                event.from.blockY == event.to.blockY &&
-                event.from.blockZ == event.to.blockZ) {
-                return
-            }
-
-            val arena = arenaManager.getArena(event.player) ?: return
-
-
-            if (arena.isWaiting()) {
-                return
-            }
-
-            if(arena.disasters.contains(FloorIsLava())) {
-                if(arena.borderService.isLocationInArenaTp(event.player)) {
-                    DisasterRegistry.addBlockToFloorIsLava(arena, event.to)
-                }
-            }
-
-            if(!arena.disasters.contains(Lag())) {
-                return
-            }
-            if (Random.nextDouble() > 0.3) return
-            when (Random.nextInt(5)) {
-                0 -> { // Simply lag effect
-                    event.isCancelled = true
-                }
-
-                1 -> { // Go back
-                    val from = event.from
-                    val to = event.to ?: return
-                    val dx = from.x - to.x
-                    val dz = from.z - to.z
-                    val back = to.clone().add(dx, 0.0, dz)
-                    event.to = back
-                }
-                2 -> { // Go forward
-                    val from = event.from
-                    val to = event.to ?: return
-                    val dx = to.x - from.x
-                    val dz = to.z - from.z
-                    val forward = to.clone().add(dx, 0.0, dz)
-                    event.to = forward
-                }
-                3 -> { // Random teleport 2 blocks away
-                    val to = event.to ?: return
-                    val randX = Random.nextDouble(-2.0, 2.0)
-                    val randZ = Random.nextDouble(-2.0, 2.0)
-                    val newLoc = to.clone().add(randX, 0.0, randZ)
-                    event.player.teleport(newLoc)
-                }
-                4 -> { //Nothing
-                }
-            }
-
+        if (event.from.block == event.to.block) {
+            return
         }
+
+        // If player is in the same block, we don't care and return
+        if (event.from.blockX == event.to.blockX &&
+            event.from.blockY == event.to.blockY &&
+            event.from.blockZ == event.to.blockZ) {
+            return
+        }
+
+        val arena = arenaManager.getArena(event.player) ?: return
+
+
+        if (arena.isWaiting()) {
+            return
+        }
+
+        if(arena.disasters.contains(FloorIsLava())) {
+            if(arena.borderService.isLocationInArenaTp(event.player)) {
+                DisasterRegistry.addBlockToFloorIsLava(arena, event.to)
+            }
+        }
+
+        if(!arena.disasters.contains(Lag())) {
+            return
+        }
+        if (Random.nextDouble() > 0.3) return
+        when (Random.nextInt(5)) {
+            0 -> { // Simply lag effect
+                event.isCancelled = true
+            }
+
+            1 -> { // Go back
+                val from = event.from
+                val to = event.to ?: return
+                val dx = from.x - to.x
+                val dz = from.z - to.z
+                val back = to.clone().add(dx, 0.0, dz)
+                event.to = back
+            }
+            2 -> { // Go forward
+                val from = event.from
+                val to = event.to ?: return
+                val dx = to.x - from.x
+                val dz = to.z - from.z
+                val forward = to.clone().add(dx, 0.0, dz)
+                event.to = forward
+            }
+            3 -> { // Random teleport 2 blocks away
+                val to = event.to ?: return
+                val randX = Random.nextDouble(-2.0, 2.0)
+                val randZ = Random.nextDouble(-2.0, 2.0)
+                val newLoc = to.clone().add(randX, 0.0, randZ)
+                event.player.teleport(newLoc)
+            }
+            4 -> { //Nothing
+            }
+        }
+
 
     }
 
