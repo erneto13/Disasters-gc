92c99ca4ac5ebb0a71177571659750e42a98987f
Imhhitt <diegovicentehernandez276@gmail.com>
2025-06-29 13:22:58 +0200

    fix: wither despawn

--- src/main/kotlin/me/hhitt/disasters/disaster/impl/Wither.kt
+++ src/main/kotlin/me/hhitt/disasters/disaster/impl/Wither.kt
@@ -4,10 +4,14 @@ import me.hhitt.disasters.arena.Arena
 import me.hhitt.disasters.disaster.Disaster
 import me.hhitt.disasters.util.Notify
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.Wither
 
 class Wither : Disaster {
+    private val activeWithers = mutableMapOf<Arena, Wither>()
+
     override fun start(arena: Arena) {
-        arena.location.world.spawnEntity(arena.location, EntityType.WITHER)
+        val wither = arena.location.world.spawnEntity(arena.location, EntityType.WITHER) as Wither
+        activeWithers[arena] = wither
         Notify.disaster(arena, "wither")
     }
 
@@ -15,5 +19,6 @@ class Wither : Disaster {
     }
 
     override fun stop(arena: Arena) {
+        activeWithers.remove(arena)?.remove()
     }
 }
