667f30b035a45685c23320974e07b33ccfb69ba9
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-10-14 23:03:55 -0700

    feat: enhance DisastersCommand with version info and improved message handling

--- src/main/kotlin/me/hhitt/disasters/command/DisastersCommand.kt
+++ src/main/kotlin/me/hhitt/disasters/command/DisastersCommand.kt
@@ -1,25 +1,42 @@
 package me.hhitt.disasters.command
 
+import me.hhitt.disasters.Disasters
 import me.hhitt.disasters.arena.ArenaManager
 import me.hhitt.disasters.sidebar.SidebarService
 import me.hhitt.disasters.storage.file.FileManager
 import me.hhitt.disasters.util.Lobby
 import me.hhitt.disasters.util.Msg
+import org.bukkit.entity.Player
 import revxrsal.commands.annotation.Command
 import revxrsal.commands.annotation.Subcommand
 import revxrsal.commands.bukkit.actor.BukkitCommandActor
 import revxrsal.commands.bukkit.annotation.CommandPermission
 
-/**
- * DisastersCommand class that handles the commands related to disasters prefix.
- *
- * @param arenaManager The ArenaManager instance used to manage arenas.
- * @param sidebarService The SidebarService instance used to manage sidebars.
- */
-
 @Command("disasters")
 @CommandPermission("disasters.admin")
-class DisastersCommand(private val arenaManager: ArenaManager, private val sidebarService: SidebarService) {
+class DisastersCommand(private val plugin: Disasters, private val arenaManager: ArenaManager, private val sidebarService: SidebarService) {
+
+    val version = plugin.pluginMeta.version
+    val customBuild = "build-2"
+
+    @Command("disasters")
+    fun disasters(actor: BukkitCommandActor) {
+        val sender = actor.sender()
+
+        if (sender !is Player) {
+            Msg.send(actor.sender(), "messages.only-players")
+            return
+        }
+
+        Msg.sendList(actor.sender(), "messages.help")
+        Msg.sendParsed(actor.sender() as Player, "<#c1c1c1><i> Version $version ($customBuild) - by erneto13")
+        Msg.sendParsed(actor.sender() as Player, "")
+    }
+
+    @Command("disasters help")
+    fun disastersHelp(actor: BukkitCommandActor) {
+        disasters(actor)
+    }
 
     @Subcommand("reload")
     fun reload(actor: BukkitCommandActor) {
@@ -27,23 +44,25 @@ class DisastersCommand(private val arenaManager: ArenaManager, private val sideb
         FileManager.get("lang")!!.reloadFile()
         arenaManager.reloadArenas()
         sidebarService.updateSidebar()
-        Msg.send(actor.sender(), "reload-success")
+        Msg.send(actor.sender(), "messages.reload-success")
     }
-
 
     @Subcommand("setspawn")
     fun setSpawn(actor: BukkitCommandActor) {
-        if(!actor.isPlayer) return
+        if (!actor.isPlayer) return
         val player = actor.asPlayer()!!
-        FileManager.get("config")!!.set("lobby.world", player.world.name)
-        FileManager.get("config")!!.set("lobby.x", player.location.x)
-        FileManager.get("config")!!.set("lobby.y", player.location.y)
-        FileManager.get("config")!!.set("lobby.z", player.location.z)
-        FileManager.get("config")!!.set("lobby.yaw", player.location.yaw)
-        FileManager.get("config")!!.set("lobby.pitch", player.location.pitch)
-        FileManager.get("config")!!.save()
+        val config = FileManager.get("config")!!
+
+        config.set("lobby.world", player.world.name)
+        config.set("lobby.x", player.location.x)
+        config.set("lobby.y", player.location.y)
+        config.set("lobby.z", player.location.z)
+        config.set("lobby.yaw", player.location.yaw)
+        config.set("lobby.pitch", player.location.pitch)
+        config.save()
+
         FileManager.reload("config")
         Lobby.setLocation()
-        Msg.send(actor.sender(), "lobby-set")
+        Msg.send(actor.sender(), "messages.lobby-set")
     }
 }
