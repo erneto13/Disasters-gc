15b24be55222edfcc39e46e21c27aac8ef386a00
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-10-14 23:03:51 -0700

    feat: integrate ArenaSetupManager and CuboidVisualizer for enhanced arena setup and visualization

--- src/main/kotlin/me/hhitt/disasters/Disasters.kt
+++ src/main/kotlin/me/hhitt/disasters/Disasters.kt
@@ -3,7 +3,9 @@ package me.hhitt.disasters
 import com.github.shynixn.mccoroutine.bukkit.registerSuspendingEvents
 import com.sk89q.worldedit.bukkit.WorldEditPlugin
 import me.hhitt.disasters.arena.ArenaManager
+import me.hhitt.disasters.arena.ArenaSetupManager
 import me.hhitt.disasters.command.ArenaCommand
+import me.hhitt.disasters.command.ArenaSetupCommand
 import me.hhitt.disasters.command.DisastersCommand
 import me.hhitt.disasters.disaster.DisasterTask
 import me.hhitt.disasters.hook.PlaceholderAPIHook
@@ -13,6 +15,7 @@ import me.hhitt.disasters.storage.data.Data
 import me.hhitt.disasters.storage.file.FileManager
 import me.hhitt.disasters.util.Filer
 import me.hhitt.disasters.util.Lobby
+import me.hhitt.disasters.visual.CuboidVisualizer
 import revxrsal.commands.Lamp
 import revxrsal.commands.bukkit.BukkitLamp
 import revxrsal.commands.bukkit.actor.BukkitCommandActor
@@ -21,63 +24,86 @@ import revxrsal.zapper.ZapperJavaPlugin
 
 class Disasters : ZapperJavaPlugin() {
 
-    // Singleton instance
     companion object {
         private lateinit var instance: Disasters
         fun getInstance(): Disasters = instance
     }
 
     private lateinit var arenaManager: ArenaManager
+    private lateinit var arenaSetupManager: ArenaSetupManager
     private lateinit var sidebarService: SidebarService
+
+    private lateinit var cuboidVisualizer: CuboidVisualizer
 
     override fun onEnable() {
         instance = this
         initStorage()
         initHooks()
+        initVisualizers()
         initSidebar()
         registerCommands()
         registerListeners()
         initDisasters()
+
+        logger.info("Disasters plugin enabled successfully!")
     }
 
     override fun onDisable() {
+        if (::cuboidVisualizer.isInitialized) {
+            cuboidVisualizer.stopAllVisualizations()
+            logger.info("All visualizations cleaned up")
+        }
+
+        logger.info("Disasters plugin disabled successfully!")
     }
 
-    private fun initHooks () {
-        // World Edit (FAWE)
+    private fun initHooks() {
         val worldEditPlugin = server.pluginManager.getPlugin("WorldEdit") as WorldEditPlugin?
         if (worldEditPlugin == null) {
             logger.severe("WorldEdit plugin not found! Disabling...")
             server.pluginManager.disablePlugin(this)
+            return
         }
 
-        // Arena Manager
         arenaManager = ArenaManager(worldEditPlugin)
+        arenaSetupManager = ArenaSetupManager()
 
-        // PlaceholderAPI
         if (server.pluginManager.getPlugin("PlaceholderAPI") != null) {
             PlaceholderAPIHook(arenaManager).register()
+            logger.info("PlaceholderAPI hook registered")
         }
     }
 
-    private fun initStorage(){
+    private fun initStorage() {
         Filer.createFolders()
         FileManager.initialize()
         Lobby.setLocation()
         Data.load()
     }
 
-    private fun initSidebar(){
+    private fun initVisualizers() {
+        cuboidVisualizer = CuboidVisualizer(this)
+        logger.info("Visualizers initialized")
+    }
+
+    private fun initSidebar() {
         sidebarService = SidebarService(arenaManager)
     }
 
     private fun registerCommands() {
         val lamp: Lamp<BukkitCommandActor> = BukkitLamp.builder(this)
             .build()
-        lamp.register(ArenaCommand(arenaManager), DisastersCommand(arenaManager, sidebarService))
+
+        lamp.register(
+            ArenaCommand(arenaManager),
+            DisastersCommand(this, arenaManager, sidebarService),
+            ArenaSetupCommand(arenaManager, arenaSetupManager, cuboidVisualizer)
+        )
+
+        logger.info("Commands registered")
     }
 
-    private fun registerListeners(){
+    private fun registerListeners() {
         server.pluginManager.registerSuspendingEvents(PlayerJoinListener(), this)
         server.pluginManager.registerSuspendingEvents(PlayerDamageListener(arenaManager), this)
         server.pluginManager.registerSuspendingEvents(PlayerDeathListener(arenaManager), this)
@@ -89,10 +115,24 @@ class Disasters : ZapperJavaPlugin() {
         server.pluginManager.registerEvents(HealthRegenListener(arenaManager), this)
         server.pluginManager.registerEvents(PlayerMoveListener(arenaManager), this)
         server.pluginManager.registerEvents(PlayerJumpListener(arenaManager), this)
+        server.pluginManager.registerEvents(ChatInputListener(), this)
+        server.pluginManager.registerEvents(ArenaEditGUIListener(), this)
+
+        server.pluginManager.registerEvents(
+            SelectionListener(arenaSetupManager, cuboidVisualizer),
+            this
+        )
+
+        logger.info("Listeners registered")
     }
 
     private fun initDisasters() {
         DisasterTask().runTaskTimer(this, 0, 20)
+        logger.info("Disaster task initialized")
     }
 
+    fun getArenaManager(): ArenaManager = arenaManager
+    fun getSetupManager(): ArenaSetupManager = arenaSetupManager
+    fun getSidebarService(): SidebarService = sidebarService
+    fun getCuboidVisualizer(): CuboidVisualizer = cuboidVisualizer
 }
