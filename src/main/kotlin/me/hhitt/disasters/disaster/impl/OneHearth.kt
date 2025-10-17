6c17570aec75da1a35c88e584e0c7ed1b468fe74
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-10-16 19:11:15 -0700

    feat: update health management in OneHearth disaster

--- src/main/kotlin/me/hhitt/disasters/disaster/impl/OneHearth.kt
+++ src/main/kotlin/me/hhitt/disasters/disaster/impl/OneHearth.kt
@@ -3,19 +3,25 @@ package me.hhitt.disasters.disaster.impl
 import me.hhitt.disasters.arena.Arena
 import me.hhitt.disasters.disaster.Disaster
 import me.hhitt.disasters.util.Notify
+import org.bukkit.attribute.Attribute
 
-class OneHearth: Disaster {
+class OneHearth : Disaster {
     override fun start(arena: Arena) {
-        arena.playing.forEach {
-            it.health = 1.0
-            it.absorptionAmount = 0.0
-        }
         Notify.disaster(arena, "one-hearth")
+        arena.playing.forEach { player ->
+            player.health = 2.0
+            player.absorptionAmount = 0.0
+        }
     }
 
     override fun pulse(time: Int) {
+
     }
 
     override fun stop(arena: Arena) {
+        arena.playing.forEach { player ->
+            val maxHealth = player.getAttribute(Attribute.MAX_HEALTH)?.value ?: 20.0
+            player.health = maxHealth
+        }
     }
 }
