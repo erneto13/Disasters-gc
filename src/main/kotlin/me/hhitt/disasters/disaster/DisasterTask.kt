4d414666354dcbd2901711ea169d997d760507d3
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-10-31 21:55:34 -0700

    refactor(disaster): simplificar lógica de pulso en DisasterTask

--- src/main/kotlin/me/hhitt/disasters/disaster/DisasterTask.kt
+++ src/main/kotlin/me/hhitt/disasters/disaster/DisasterTask.kt
@@ -2,19 +2,15 @@ package me.hhitt.disasters.disaster
 
 import org.bukkit.scheduler.BukkitRunnable
 
-/**
- * DisasterTask is a BukkitRunnable that pulses all disasters every second.
- **/
-
+/** DisasterTask is a BukkitRunnable that pulses all disasters every second. */
 class DisasterTask : BukkitRunnable() {
     private var time = 0
+
     override fun run() {
-        if(time >= 4) {
-            DisasterRegistry.pulseAll(time)
-        }
+        DisasterRegistry.pulseAll(time)
         time++
-        if(time >= 100) {
-            time = 3
+        if (time >= 10000) {
+            time = 0
         }
     }
 }
