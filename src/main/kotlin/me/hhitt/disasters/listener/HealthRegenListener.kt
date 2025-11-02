45ae40112271718cd627872c4bcf82c61a568ae0
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-11-01 17:18:28 -0700

    fix(listener): no más regeneración de vida mientras juega

--- src/main/kotlin/me/hhitt/disasters/listener/HealthRegenListener.kt
+++ src/main/kotlin/me/hhitt/disasters/listener/HealthRegenListener.kt
@@ -1,29 +1,21 @@
 package me.hhitt.disasters.listener
 
 import me.hhitt.disasters.arena.ArenaManager
-import me.hhitt.disasters.disaster.impl.OneHearth
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityRegainHealthEvent
 
-class HealthRegenListener(private val arenaManager: ArenaManager): Listener {
+class HealthRegenListener(private val arenaManager: ArenaManager) : Listener {
 
     @EventHandler
     fun onHealthRegen(event: EntityRegainHealthEvent) {
-        // If the entity is not a player, we don't care and return
         if (event.entity !is Player) {
             return
         }
 
         val player = event.entity as Player
 
-        // If a player is in an arena
-        arenaManager.getArena(player)?.let { arena ->
-            if(arena.disasters.contains(OneHearth())) {
-                event.isCancelled = true
-            }
-        }
+        arenaManager.getArena(player)?.let { arena -> event.isCancelled = true }
     }
-
 }
